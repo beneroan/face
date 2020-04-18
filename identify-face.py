9abdd996c1edf36b0b6641bb6216bb0b0a78d341
Ben Taylor <bentaylor@bentaylor.me>
2020-04-18 01:23:17 -0500

    identify face

---
+++ identify-face.py
@@ -0,0 +1,38 @@
+import asyncio
+import io
+import glob
+import os
+import sys
+import time
+import uuid
+import requests
+from urllib.parse import urlparse
+from io import BytesIO
+from PIL import Image, ImageDraw
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
+
+KEY = os.environ['FACE_SUBSCRIPTION_KEY']
+ENDPOINT = os.environ['FACE_ENDPOINT']
+
+face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+
+PERSON_GROUP_ID = 'hacktams2020-person-group'
+
+def identify (file):
+   image = open(file, 'r+b')
+   face_ids = []
+   faces = face_client.face.detect_with_stream(image)
+   for face in faces:
+      face_ids.append(face.face_id)
+   
+   results = face_client.face.identify(face_ids, PERSON_GROUP_ID);
+   print('Identifying faces')
+   if not results:
+      print('No one identified')
+   for person in results:
+      print(person.candidates[0])
+      print('identified ' + os.path.basename(image.name) + '. Confidence: ' + str(person.candidates[0].confidence))
+
+identify(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test.jpg'))
